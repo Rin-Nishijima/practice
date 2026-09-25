25b90cd8033cd2ce6f2923db24f40d4183104a78
Rin-Nishijima <jeanlucpicard47@yahoo.com>
2026-09-25 19:24:06 +0900

    number -> isbn

--- Book.java
+++ Book.java
@@ -1,5 +1,5 @@
 public class Book extends TangibleAsset {
-  private String number;
+  private String isbn;
   public Book(String name, int price, String color, String isbn) {
     super(name, price);
     this.isbn = isbn;
